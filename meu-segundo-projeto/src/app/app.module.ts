da3b33da6e3534a2b78ff00ba481a291e57b7b6d
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2023-05-17 20:58:56 -0300

    criei outro modulo

--- meu-segundo-projeto/src/app/app.module.ts
+++ meu-segundo-projeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TituloComponent } from './titulo/titulo.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TituloComponent
+    TituloComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
