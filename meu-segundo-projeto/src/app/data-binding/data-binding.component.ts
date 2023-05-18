b4ff78106f90a561069661022b51e3ff67a8c5e4
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2023-05-17 22:07:21 -0300

    property binding

--- meu-segundo-projeto/src/app/data-binding/data-binding.component.ts
+++ meu-segundo-projeto/src/app/data-binding/data-binding.component.ts
@@ -8,9 +8,24 @@ import { Component } from '@angular/core';
 export class DataBindingComponent {
   protected nome: string;
   protected idade: number;
+  protected imgSrc: string;
+  protected imgAlt: string;
+  protected buttonDisabled: boolean;
 
   constructor() {
     this.nome = 'Arthur';
     this.idade = 19;
+    this.buttonDisabled = false;
+    this.imgSrc = 'https://i.pinimg.com/564x/17/6e/9b/176e9b666a2461b8ed3fef458a64dbb7.jpg';
+    this.imgAlt = 'Cenário Pixel Art';
   }
+
+  onOff(): boolean {
+    if (this.buttonDisabled) {
+      return this.buttonDisabled = false;
+    } else {
+      return this.buttonDisabled = true;
+    }
+  }
+
 }
