d595bff61d350edf3e4818f6afd1ddf167e088eb
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2023-05-23 11:44:28 -0300

    ngIf

--- meu-segundo-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ meu-segundo-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretivas-estruturais.component.scss']
 })
 export class DiretivasEstruturaisComponent {
+  protected condition: boolean;
 
+  constructor() {
+    this.condition = true;
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      if(this.condition === true) {
+        this.condition = false;
+      } else {
+        this.condition = true;
+      }
+    }, 2000);
+  }
 }
