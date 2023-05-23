4c59088c51edafa7f6a05c1784b19397c2f77f42
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2023-05-23 10:47:57 -0300

    diretivas estruturais

--- meu-segundo-projeto/src/app/app.module.ts
+++ meu-segundo-projeto/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { TituloComponent } from './titulo/titulo.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
+import { DiretivasEstruturaisComponent } from './diretivas-estruturais/diretivas-estruturais.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TituloComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    DiretivasEstruturaisComponent
   ],
   imports: [
     BrowserModule,
