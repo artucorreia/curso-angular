78be7bcddea16eb4c99fec060ad6fcb066c3638d
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-14 13:00:46 -0300

    app-todo

--- app-todo/src/app/app.module.ts
+++ app-todo/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    HeaderComponent,
-    FooterComponent,
-    ButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
