5bf76b36cedde3e5abb883178d963d5ed31fc3f9
Arthur Correia <arthurcorria0109@gmail.com>
2023-08-15 13:30:39 -0300

    test1

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Task } from './interfaceTask';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
   public hasTask = true;
+  public test = 'coisas leves';
+  constructor () { 
+
+  }
+
+  ngOnInit(): managerTask {
+    return new managerTask();
+  }
+  
+  teste(name: string) {
+    
+  }
+}
+
+export class Task {
+  name: string;
+  status: boolean;
+  
+  constructor(name: string) {
+    this.name = name;
+    this.status = false;
+  }
+}
+
+export class managerTask {
+  public tasks: Task[];
+
+  constructor() { 
+    this.tasks = [];
+  }
+  
+  addTask(name: string): {} {
+    return this.tasks.push(new Task(name));
+  }
 }
