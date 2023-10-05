7e946f4310915dd1af3f97833a23d737db4b7519
Arthur Correia <arthurcorria0109@gmail.com>
2023-10-04 22:34:45 -0300

    Update food-list.service.ts

--- meu-segundo-projeto/src/app/services/food-list.service.ts
+++ meu-segundo-projeto/src/app/services/food-list.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Event } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodListService {
   
+  public emitEvent = new EventEmitter();
+
   private list: string[] = [
     'x-bacon',
     'feijão',
@@ -16,4 +19,8 @@ export class FoodListService {
   public foodList(): string[] { return this.list; }
 
   public addNewFood(newFood: string): string[] | number { return this.list.push(newFood); }
+
+  public eventAlert() {
+    
+  }
 }
