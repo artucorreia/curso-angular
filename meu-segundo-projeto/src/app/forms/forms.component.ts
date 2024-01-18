6b3e4bddab7ebad50f4a25f14d014ee05515d31f
Arthur Correia <arthurcorria0109@gmail.com>
2024-01-18 18:39:20 -0300

    select
    usando o ngFor no select

--- meu-segundo-projeto/src/app/forms/forms.component.ts
+++ meu-segundo-projeto/src/app/forms/forms.component.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class FormsComponent {
 
+  public foods: {name: string, price: number}[] = [
+    {
+      name: 'X-Salada', 
+      price: 11
+    },
+    {
+      name: 'X-Bacon', 
+      price: 18
+    },
+    {
+      name: 'X-Frango', 
+      price: 15
+    }
+  ]
+
+  constructor() { }
+
   public submitForm(form: {}) {
     console.log(form);
   }
