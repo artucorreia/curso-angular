b0a16b36c8b1304d3f5e72cf38fa8f51b8677355
Arthur Correia <arthurcorria0109@gmail.com>
2024-01-30 12:46:30 -0300

    router

--- routes/src/app/pages/page-error/page-error.component.ts
+++ routes/src/app/pages/page-error/page-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-error',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-error.component.scss']
 })
 export class PageErrorComponent {
+  constructor(private router: Router) { }
 
+  ngOnInit() {
+    setInterval(()=> {
+      this.router.navigate(['/']);
+      // this.router.navigateByUrl('/');
+    }, 5000)
+  }
 }
