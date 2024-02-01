c5657a83950836675cddda8aef72d124770927c9
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-01 09:27:40 -0300

    lazy loading
    criei o módulo e seu componente

---
+++ routes/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Component
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
