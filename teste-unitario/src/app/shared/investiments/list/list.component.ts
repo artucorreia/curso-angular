9f73b4ad6521603d168b5eb2c81ebaeca3353bb1
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-11 14:09:16 -0300

    testes unitários
    testando um componente dentro do outro

--- teste-unitario/src/app/shared/investiments/list/list.component.ts
+++ teste-unitario/src/app/shared/investiments/list/list.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+// Model
+import { Investiments } from '../model/investiments';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  
+  public investiments: Investiments[] = [
+    {
+    'name': 'Itaú',
+    'value': 100
+    },
+    {
+    'name': 'Will',
+    'value': 100
+    },
+    {
+    'name': 'Nubank',
+    'value': 100
+    },
+    {
+    'name': 'Inter',
+    'value': 100
+    }
+  ];
 
+  constructor() { }
 }
