249b2085b63106c84d4a350ec2c3291686dc763e
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-16 13:03:33 -0300

    canActivate

--- guards/src/app/app-routing.module.ts
+++ guards/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Componente Pages
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+import { CanActivateGuard } from './shared/guards/can-activate.guard';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, pathMatch:'full'
+  },
+  {
+    path: 'account', component: AccountComponent, canActivate: [CanActivateGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ guards/src/app/shared/guards/can-activate.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActivateGuard {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+    ): 
+    Observable<boolean | UrlTree> 
+    | Promise<boolean | UrlTree> 
+    | boolean 
+    | UrlTree {
+
+    if (
+      route.queryParams['account'] === 'admin' &&
+      route.queryParams['password'] === '1234'
+    ) {
+      return true;
+    }
+    return false;
+  }
+  
+}
